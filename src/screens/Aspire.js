520d6790214b58ae68fb5594ebbb1d018a9d6f9e
aspirenocoder <letspartner@spigroup.in>
2024-09-20 12:16:14 +0530

    Added aspire website

---
+++ src/screens/Aspire.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+const Aspire = () => {
+  const [htmlContent, setHtmlContent] = useState("");
+
+  useEffect(() => {
+    fetch(`/aspire.html`)
+      .then((response) => response.text())
+      .then((data) => {
+        setHtmlContent(data);
+      })
+      .catch((error) => console.error("Error loading HTML:", error));
+  }, []);
+
+  return (
+    <div
+      style={{
+        margin: "0px",
+        padding: "0px",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src="/aspire.html"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        title="HTML Renderer"
+      />
+    </div>
+  );
+};
+
+export default Aspire;
