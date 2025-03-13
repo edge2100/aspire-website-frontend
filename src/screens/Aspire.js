928de41269c4f61f276906deaa0a971ce1676026
apple <apple@apples-MacBook-Pro.local>
2025-03-13 12:40:23 +0530

    Solved production issues for netlify

--- src/screens/Aspire.js
+++ src/screens/Aspire.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 const Aspire = () => {
-  const [htmlContent, setHtmlContent] = useState("");
+  // const [htmlContent, setHtmlContent] = useState("");
 
-  useEffect(() => {
-    fetch(`/aspire.html`)
-      .then((response) => response.text())
-      .then((data) => {
-        setHtmlContent(data);
-      })
-      .catch((error) => console.error("Error loading HTML:", error));
-  }, []);
+  // useEffect(() => {
+  //   fetch(`/aspire.html`)
+  //     .then((response) => response.text())
+  //     .then((data) => {
+  //       setHtmlContent(data);
+  //     })
+  //     .catch((error) => console.error("Error loading HTML:", error));
+  // }, []);
 
   return (
     <div
