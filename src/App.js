271096ddce06f95e8a9c8b0cc5fcf651cad728ba
apple <apple@apples-MacBook-Pro.local>
2025-04-07 15:38:00 +0530

    Added clarity and google analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Certificate from "./screens/Certificate";
 import Aspire from "./screens/Aspire";
+import Clarity from '@microsoft/clarity';
+Clarity.init("oxf1beiydw");
 
 const App = () => {
   return (
